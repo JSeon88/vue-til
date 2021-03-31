dc9270ea84420a927d8664ea00e81e2b23fca6a9
seonyoung-jeon <seonyoung.jeon@nhnsoft.com>
2021-03-31 11:34:35 +0900

    login, validate

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="username">id: </label>
+			<input id="username" type="text" v-model="username" />
+		</div>
+		<div>
+			<label for="password">pw: </label>
+			<input id="password" type="text" v-model="password" />
+		</div>
+		<button :disabled="!isUserNameValid || !password" type="submit">
+			로그인
+		</button>
+		<p>{{ logMessage }}</p>
+	</form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
+
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+			logMessage: '',
+		};
+	},
+	computed: {
+		isUserNameValid() {
+			return validateEmail(this.username);
+		},
+	},
+	methods: {
+		async submitForm() {
+			try {
+				const userData = {
+					username: this.username,
+					password: this.password,
+				};
+				const { data } = await loginUser(userData);
+				this.logMessage = `${data.user.username} 님 환영합니다.`;
+			} catch (error) {
+				this.logMessage = error.response.data;
+			} finally {
+				this.initForm();
+			}
+		},
+		initForm() {
+			this.username = '';
+			this.password = '';
+		},
+	},
+};
+</script>
+
+<style></style>
