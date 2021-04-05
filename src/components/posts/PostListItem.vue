a93e18000a2330ed44c9597741de20774d0696ca
seonyoung-jeon <seonyoung.jeon@nhnsoft.com>
2021-04-05 21:24:32 +0900

    지역 filter, 전역 filter

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -3,7 +3,7 @@
 		<li>
 			<div>제목 : {{ postItem.title }}</div>
 			<div>내용 : {{ postItem.contents }}</div>
-			<div>등록일 : {{ postItem.createdAt }}</div>
+			<div>등록일 : {{ postItem.createdAt | formatDate }}</div>
 			<button @click="routeEditPage">수정</button>
 			<button @click="deleteItem">삭제</button>
 		</li>
@@ -30,6 +30,12 @@ export default {
 			this.$router.push(`/post/${this.postItem._id}`);
 		},
 	},
+	// 지역 필터 처리
+	// filters: {
+	// 	formatDate(value) {
+	// 		return new Date(value);
+	// 	},
+	// },
 };
 </script>
 
